945fd59938fcc9a4af87c5a683048b1873a621d3
Dmcdominic <dmcdominic@gmail.com>
2019-03-05 22:00:07 -0500

    Added paint/turpentine indicator icons, and added sample npc's to the level scene.

--- The Painter/Assets/Scripts/Objects/erasable.cs
+++ The Painter/Assets/Scripts/Objects/erasable.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class erasable : MonoBehaviour {
 
+	// Public fields
 	public List<GameObject> to_erase;
+	public SpriteRenderer indicator_sprite;
 
+	// Private vars
+	private bool show_indicator = false;
+
+
+	// Init the indicator as hidden
+	private void Start() {
+		set_indicator_alpha(0);
+	}
+
+	// Regularly adjust the opacity of the indicator
+	private void Update() {
+		float alpha_incr = (show_indicator ? 1f : -1f) / paintable.indicator_fade_time * Time.deltaTime;
+		adjust_indicator_alpha(alpha_incr);
+	}
 
 	// Call this to erase all the corresponding objects
 	public void erase() {
@@ -16,5 +32,32 @@ public class erasable : MonoBehaviour {
                 FindObjectOfType<sound_manager>().playTurpentineSound();
             }
 		}
+	}
+
+	// Show paintable indicator when touching player
+	private void OnTriggerEnter2D(Collider2D collision) {
+		painter Painter = collision.gameObject.GetComponent<painter>();
+		if (Painter != null) {
+			show_indicator = true;
+		}
+	}
+
+	// Hide paintable indicator when not touching player
+	private void OnTriggerExit2D(Collider2D collision) {
+		painter Painter = collision.gameObject.GetComponent<painter>();
+		if (Painter != null) {
+			show_indicator = false;
+		}
+	}
+
+	// Set the alpha of the indicator sprite
+	private void set_indicator_alpha(float a) {
+		a = Mathf.Clamp(a, 0, 1);
+		Color col = indicator_sprite.color;
+		indicator_sprite.color = new Color(col.r, col.g, col.b, a);
+	}
+	private void adjust_indicator_alpha(float delta) {
+		Color col = indicator_sprite.color;
+		set_indicator_alpha(col.a + delta);
 	}
 }
--- The Painter/Assets/Scripts/Objects/paintable.cs
+++ The Painter/Assets/Scripts/Objects/paintable.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class paintable : MonoBehaviour {
 
-	//public Sprite indicator_sprite;
+	// Public fields
 	public List<GameObject> to_paint_in;
+	public SpriteRenderer indicator_sprite;
 
-	//private bool show_indicator = false;
+	// Private vars
+	private bool show_indicator = false;
 
+	// Static settings
+	public static float indicator_fade_time = 0.4f;
+
+
+	// Init the indicator as hidden
+	private void Start() {
+		set_indicator_alpha(0);
+	}
+
+	// Regularly adjust the opacity of the indicator
+	private void Update() {
+		float alpha_incr = (show_indicator ? 1f : -1f) / indicator_fade_time * Time.deltaTime;
+		adjust_indicator_alpha(alpha_incr);
+	}
 
 	// Call this to paint in all the corresponding objects
 	public void paint_in() {
@@ -21,19 +37,30 @@ public class paintable : MonoBehaviour {
 		}
 	}
 
-	//// Show paintable indicator when touching player
-	//private void OnTriggerEnter2D(Collider2D collision) {
-	//	painter Painter = collision.gameObject.GetComponent<painter>();
-	//	if (Painter != null) {
-	//		show_indicator = true;
-	//	}
-	//}
+	// Show paintable indicator when touching player
+	private void OnTriggerEnter2D(Collider2D collision) {
+		painter Painter = collision.gameObject.GetComponent<painter>();
+		if (Painter != null) {
+			show_indicator = true;
+		}
+	}
 
-	//// Hide paintable indicator when not touching player
-	//private void OnTriggerExit2D(Collider2D collision) {
-	//	painter Painter = collision.gameObject.GetComponent<painter>();
-	//	if (Painter != null) {
-	//		show_indicator = false;
-	//	}
-	//}
+	// Hide paintable indicator when not touching player
+	private void OnTriggerExit2D(Collider2D collision) {
+		painter Painter = collision.gameObject.GetComponent<painter>();
+		if (Painter != null) {
+			show_indicator = false;
+		}
+	}
+
+	// Set the alpha of the indicator sprite
+	private void set_indicator_alpha(float a) {
+		a = Mathf.Clamp(a, 0, 1);
+		Color col = indicator_sprite.color;
+		indicator_sprite.color = new Color(col.r, col.g, col.b, a);
+	}
+	private void adjust_indicator_alpha(float delta) {
+		Color col = indicator_sprite.color;
+		set_indicator_alpha(col.a + delta);
+	}
 }
